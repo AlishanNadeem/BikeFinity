1dcdc19867416a24201fd1938f262714d5366865
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-02-19 16:03:56 +0500

    Localhost added.

--- client/app/config.js
+++ client/app/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://server-deploy-bikefinity.herokuapp.com";
+// export const BASE_URL = "http://server-deploy-bikefinity.herokuapp.com";
+
+export const BASE_URL = "http://10.0.2.2:5000";
