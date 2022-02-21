3dabdf18331e50a870211ebfc76745e7b0d3418c
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-02-22 03:29:29 +0500

    Update Events.js
    Temporary logout added in events module for testing purpose.

--- client/app/screens/Events.js
+++ client/app/screens/Events.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { View, Text } from 'react-native';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
+import { LogOut } from "../redux/actions/authAction";
 
 const Events = () => {
+
+    const dispatch = useDispatch();
+
     return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Events</Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+            <TouchableOpacity onPress={() => dispatch(LogOut())}>
+                <Text>LOGOUT</Text>
+            </TouchableOpacity>
         </View>
     );
 };
