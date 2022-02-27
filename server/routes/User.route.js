9caf7c0d4340db863fdc8931adee8effc7bf561c
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-02-28 03:56:52 +0500

    Update User.route.js

--- server/routes/User.route.js
+++ server/routes/User.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const user_controller = require('../controllers/User.controller');
+const review_controller = require('../controllers/Review.controller');
 const { AuthVerifier } = require('../middleware/AuthVerifier');
 
 router.post('/postAd', user_controller.postAd);
@@ -9,5 +10,9 @@ router.post('/postAd', user_controller.postAd);
 router.get('/getAds', AuthVerifier, user_controller.getAds);
 
 router.get('/getAd/:id', user_controller.getAd);
+
+router.post('/postReview', AuthVerifier, review_controller.postReview);
+
+router.get('/getReviews/:id', review_controller.getReviews);
 
 module.exports = router;
