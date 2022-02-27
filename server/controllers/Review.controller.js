1dd1c74fe95ae39e8bf6bd5555683910fdc5ca8e
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-02-28 03:56:39 +0500

    Create Review.controller.js

---
+++ server/controllers/Review.controller.js
@@ -0,0 +1,24 @@
+const Review = require('../models/Review.model');
+
+exports.postReview = ((req, res, next) => {
+    let review = new Review({
+        review: req.body.review,
+        rating: req.body.rating,
+        bikeId: req.body.bikeId,
+        userId: req.decoded.id,
+    });
+
+    review.save((err) => {
+        if (err) return next(err);
+
+        res.send("Review posted successfully");
+    })
+});
+
+exports.getReviews = ((req, res, next) => {
+    Review.findById({ _id: req.params.id }, (err, reviews) => {
+        if (err) return next(err);
+
+        res.send(reviews);
+    })
+});
