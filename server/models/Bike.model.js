034bf534c56eada0bf988ee367b962af9ee34549
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-03-06 01:34:29 +0500

    Update Bike.model.js

--- server/models/Bike.model.js
+++ server/models/Bike.model.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 let bikeSchema = new Schema({
     make: { type: String },
     model: { type: String },
-    engine: { type: Number }
+    engine: { type: Number },
+    image: {type: String},
+    type: {type: String},
+    averageRating: {type: Number},
 });
 
 module.exports = mongoose.model('Bike', bikeSchema);
