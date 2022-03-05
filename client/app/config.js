5395ffa8f833195b636693f755e758b60a3cc557
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-03-06 01:34:48 +0500

    Update config.js

--- client/app/config.js
+++ client/app/config.js
@@ -1,3 +1,9 @@
 // export const BASE_URL = "http://server-deploy-bikefinity.herokuapp.com";
 
 export const BASE_URL = "http://10.0.2.2:5000";
+
+export const PRIMARY_COLOR = "#011627";
+
+export const SECONDARY_COLOR = "#CA054D";
+
+export const STAR_COLOR = "#ffd23f"
