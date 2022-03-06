a6618d138162c8668794faa543fa4590ac655a4b
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-03-07 00:49:41 +0500

    Update ViewReview.js
    Added flatlist.

--- client/app/screens/ViewReview.js
+++ client/app/screens/ViewReview.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, ActivityIndicator, StyleSheet, Image } from 'react-native';
-import { Divider } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import MIcon from 'react-native-vector-icons/MaterialIcons'; // have to change the name of object
-import Button from "../components/Button";
+import { View, Text, ScrollView, ActivityIndicator, StyleSheet, Image, FlatList } from 'react-native';
+import { Avatar } from 'react-native-paper';
 import StarRating from 'react-native-star-rating';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -19,6 +16,37 @@ const ViewReview = () => {
 
     const [bike, setBike] = useState({});
     const [isLoaded, setIsLoaded] = useState(false);
+
+    const [data, setData] = useState([
+        {
+            _id: 1,
+            review: 'good bike',
+            user: 'Alishan Nadeem',
+            days: '6 days',
+            rating: 5,
+        },
+        {
+            _id: 2,
+            review: 'fuel average of that bike is amazing',
+            user: 'Asim Ebrahim',
+            days: '2 days',
+            rating: 4,
+        },
+        {
+            _id: 3,
+            review: 'Fantasitic bike.',
+            user: 'Anas Saleem',
+            days: '6 days',
+            rating: 5,
+        },
+        {
+            _id: 4,
+            review: 'Suspension is not good.',
+            user: 'Umer ali',
+            days: '1 days',
+            rating: 1,
+        },
+    ])
 
     useEffect(() => {
         getBike();
@@ -42,6 +70,41 @@ const ViewReview = () => {
                 console.log(error)
             })
     }
+
+    const renderItem = ({ item }) => (
+        <View style={{ height: 150, marginVertical: 2, padding: 2 }}>
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 0.4, flexDirection: 'row' }}>
+                    <View style={{ flex: 0.15}}>
+                        <Avatar.Text size={40} label="XD" style={{ backgroundColor: 'grey' }} color='white' />
+                    </View>
+                    <View style={{ flex: 0.45 }}>
+                        <View>
+                            <Text style={{ color: 'black', fontSize: 15 }}>{item.user}</Text>
+                        </View>
+                        <View style={{ width: '50%', marginTop: 6 }}>
+                            <StarRating
+                                disabled={true}
+                                maxStars={5}
+                                rating={item.rating}
+                                starSize={12}
+                                emptyStarColor={STAR_COLOR}
+                                fullStarColor={STAR_COLOR}
+                            />
+                        </View>
+                    </View>
+                    <View style={{ flex: 0.45, alignItems: 'flex-end' }}>
+                        <View>
+                            <Text style={{ color: 'grey', fontSize: 12 }}>{item.days} ago</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={{ flex: 0.6 }}>
+                    <Text style={{color: 'black'}}>{item.review}</Text>
+                </View>
+            </View>
+        </View>
+    );
 
     return (
         isLoaded ?
@@ -75,9 +138,14 @@ const ViewReview = () => {
                     </View>
 
                 </View>
-                <View style={{ flex: 0.48, padding: 10 }}>
+                <View style={{ flex: 0.5, paddingHorizontal: 10, backgroundColor: 'white' }}>
+                    <FlatList
+                        keyExtractor={(item) => item._id}
+                        data={data}
+                        renderItem={renderItem}
+                    />
                 </View>
-                <View style={{ flex: 0.07, backgroundColor: 'white', flexDirection: 'row' }}>
+                <View style={{ flex: 0.1, backgroundColor: 'blue', flexDirection: 'row' }}>
                 </View>
             </View> :
             <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
