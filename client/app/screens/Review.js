e0078ad73b470884851e66923c8e51508013eff6
Alishan Nadeem <alishan.nadeem22@gmail.com>
2022-03-16 15:11:11 +0500

    Update Review.js
    Made it responsive using flex boxes.

--- client/app/screens/Review.js
+++ client/app/screens/Review.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image, ActivityIndicator, useWindowDimensions } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import StarRating from 'react-native-star-rating';
 
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute, useIsFocused } from '@react-navigation/native';
 import Axios from 'axios';
 
 import { BASE_URL, PRIMARY_COLOR, SECONDARY_COLOR, STAR_COLOR } from '../config';
@@ -12,6 +12,8 @@ import Button from '../components/Button';
 const Review = () => {
 
     const navigation = useNavigation();
+    const isFocused = useIsFocused();
+    const { height, width } = useWindowDimensions();
 
     const [make, setMake] = useState([]);
     const [selectedMake, setSelectedMake] = useState('');
@@ -30,7 +32,7 @@ const Review = () => {
     useEffect(() => {
         getBikeMake();
         getTopRatedBikes();
-    }, []);
+    }, [isFocused]);
 
     const getBikeMake = () => {
         Axios.get(`${BASE_URL}/bikefinity/bike/make`)
@@ -66,7 +68,7 @@ const Review = () => {
     }
 
     const onClickGo = (id) => {
-        setLoading(true);
+        // setLoading(true);
         navigation.navigate('ViewReview', {
             id: id
         });
@@ -74,20 +76,20 @@ const Review = () => {
 
     const renderItem = ({ item }) => (
         <TouchableOpacity
-            style={{ borderRadius: 8, flexDirection: 'column', width: 160, backgroundColor: 'white', margin: 4, height: 175, padding: 5 }}
+            style={{ borderRadius: 8, flex: 1, flexDirection: 'column', width: width / 2.6, backgroundColor: 'white', margin: 4, padding: 5 }}
             onPress={() => {
                 onClickGo(item._id)
             }}
             // delayPressIn={80}
             activeOpacity={1}
         >
-            <View style={{ flex: 0.5, backgroundColor: 'skyblue' }}>
-                <Image source={{ uri: `${item.image}` }} style={{ height: 90, width: '100%' }} resizeMode='cover' />
+            <View style={{ flex: 0.6, backgroundColor: 'skyblue' }}>
+                <Image source={{ uri: `${item.image}` }} style={{ flex: 1, width: '100%' }} resizeMode='cover' />
             </View>
-            <View style={{ flex: 0.5, padding: 5 }}>
-                <Text style={{ color: 'black', marginTop: 15 }}>{item.make}</Text>
-                <Text style={{ color: 'black', marginTop: 0, fontWeight: 'bold' }}>{item.model}</Text>
-                <View style={{ marginTop: 5, width: '70%' }}>
+            <View style={{ flex: 0.4, padding: 5, justifyContent: 'space-evenly' }}>
+                <Text style={{ color: 'black' }}>{item.make}</Text>
+                <Text style={{ color: 'black', fontWeight: 'bold' }}>{item.model}</Text>
+                <View style={{ width: '70%' }}>
                     <StarRating
                         disabled={true}
                         maxStars={5}
@@ -103,104 +105,108 @@ const Review = () => {
 
     return (
         <View style={{ flex: 1, backgroundColor: 'white', padding: 15 }}>
-            <View>
+            <View style={{ flex: 0.1, justifyContent: 'center' }}>
                 <Text style={{ fontSize: 30, color: 'black', fontWeight: 'bold' }}>Reviews</Text>
             </View>
-            <View style={{ marginTop: 30 }}>
-                <Text style={{ color: 'black' }}>Make</Text>
-                <View style={{ marginTop: 5, borderBottomWidth: 1 }}>
-                    <Picker
-                        mode="dialog"
-                        selectedValue={selectedMake}
-                        onValueChange={(itemValue, itemIndex) => {
-                            if (itemIndex === 0) {
-                                setEmptyMake(true);
-                                setSelectedMake("");
-                                setSelectedModel("");
-                                setBikeDetails([]);
-                                setBikeId("");
-                            } else {
-                                setBikeDetails([]);
-                                setBikeId("");
-                                setSelectedMake(itemValue);
-                                getBikeDetails(itemValue);
-                                setEmptyMake(false);
+            <View style={{ flex: 0.5, justifyContent: 'space-evenly' }}>
+                <View>
+                    <Text style={{ color: 'black' }}>Make</Text>
+                    <View style={{ marginTop: 5, borderBottomWidth: 1 }}>
+                        <Picker
+                            mode="dialog"
+                            selectedValue={selectedMake}
+                            onValueChange={(itemValue, itemIndex) => {
+                                if (itemIndex === 0) {
+                                    setEmptyMake(true);
+                                    setSelectedMake("");
+                                    setSelectedModel("");
+                                    setBikeDetails([]);
+                                    setBikeId("");
+                                } else {
+                                    setBikeDetails([]);
+                                    setBikeId("");
+                                    setSelectedMake(itemValue);
+                                    getBikeDetails(itemValue);
+                                    setEmptyMake(false);
+                                }
+                            }}
+                        >
+                            <Picker.Item label='Select Make' value="" />
+                            {
+                                make.map((make, index) => {
+                                    return (<Picker.Item label={make} value={make} key={index} />);
+                                })
                             }
-                        }}
-                    >
-                        <Picker.Item label='Select Make' value="" />
-                        {
-                            make.map((make, index) => {
-                                return (<Picker.Item label={make} value={make} key={index} />);
-                            })
-                        }
-                    </Picker>
+                        </Picker>
+                    </View>
+                    <View style={styles.errorContainer}>
+                        {emptyMake ? <Text style={styles.errorLabel}>Please select make!</Text> : null}
+                    </View>
                 </View>
-                <View style={styles.errorContainer}>
-                    {emptyMake ? <Text style={styles.errorLabel}>Please select make!</Text> : null}
-                </View>
-            </View>
-            <View style={{ marginTop: 5 }}>
-                <Text style={{ color: 'black' }}>Model</Text>
-                <View style={{ marginTop: 5, borderBottomWidth: 1 }}>
-                    <Picker
-                        mode="dialog"
-                        selectedValue={selectedModel}
-                        onValueChange={(itemValue, itemIndex) => {
-                            if (itemIndex === 0) {
-                                setEmptyModel(true);
-                                setSelectedModel("");
-                                setBikeId("");
-                            } else {
-                                setSelectedModel(itemValue);
-                                setBikeId(bikeDetails[itemIndex - 1]._id)
-                                setEmptyModel(false);
+                <View style={{ marginTop: 5 }}>
+                    <Text style={{ color: 'black' }}>Model</Text>
+                    <View style={{ marginTop: 5, borderBottomWidth: 1 }}>
+                        <Picker
+                            mode="dialog"
+                            selectedValue={selectedModel}
+                            onValueChange={(itemValue, itemIndex) => {
+                                if (itemIndex === 0) {
+                                    setEmptyModel(true);
+                                    setSelectedModel("");
+                                    setBikeId("");
+                                } else {
+                                    setSelectedModel(itemValue);
+                                    setBikeId(bikeDetails[itemIndex - 1]._id)
+                                    setEmptyModel(false);
+                                }
+                            }}
+                        >
+                            <Picker.Item label='Select Model' value="" />
+                            {
+                                bikeDetails.map((bike, index) => {
+                                    return (<Picker.Item label={bike.model} value={bike.model} key={index} />);
+                                })
                             }
-                        }}
-                    >
-                        <Picker.Item label='Select Model' value="" />
-                        {
-                            bikeDetails.map((bike, index) => {
-                                return (<Picker.Item label={bike.model} value={bike.model} key={index} />);
-                            })
-                        }
-                    </Picker>
+                        </Picker>
+                    </View>
+                    <View style={styles.errorContainer}>
+                        {emptyModel ? <Text style={styles.errorLabel}>Please select model!</Text> : null}
+                    </View>
                 </View>
-                <View style={styles.errorContainer}>
-                    {emptyModel ? <Text style={styles.errorLabel}>Please select model!</Text> : null}
-                </View>
-            </View>
-            <View style={{ marginTop: 5, height: 60, alignItems: 'flex-end', justifyContent: 'center' }}>
-                <View style={{ width: '30%' }}>
-                    <TouchableOpacity onPress={() => onClickGo(bikeId)} disabled={loading ? true : false}>
-                        <Button name="Go" loading={loading} />
-                    </TouchableOpacity>
+                <View style={{ marginTop: 5, height: 60, alignItems: 'flex-end', justifyContent: 'center' }}>
+                    <View style={{ width: '30%' }}>
+                        <TouchableOpacity onPress={() => onClickGo(bikeId)} disabled={loading ? true : false}>
+                            <Button name="Go" loading={loading} />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-            <View style={{ marginTop: 5 }}>
-                <Text style={{ color: PRIMARY_COLOR, fontSize: 24 }}>Top Rated Bikes</Text>
-            </View>
-            <View style={{ marginTop: 2 }}>
-                <Text>Here are some of the most rated bikes.</Text>
-            </View>
-            <View style={{ marginTop: 6, height: 185, backgroundColor: '#F7F7F7' }}>
-                {
-                    dataIsLoaded ?
-                        (
-                            <FlatList
-                                keyExtractor={(item) => item._id}
-                                data={data}
-                                renderItem={renderItem}
-                                horizontal={true}
-                                showsHorizontalScrollIndicator={false}
-                            />
-                        ) :
-                        (
-                            <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-                                <ActivityIndicator size="large" color='#CA054D' />
-                            </View>
-                        )
-                }
+            <View style={{ flex: 0.4, justifyContent: 'space-evenly' }}>
+                <View style={{ flex: 0.2, justifyContent: 'center' }}>
+                    <Text style={{ color: PRIMARY_COLOR, fontSize: 24 }}>Top Rated Bikes</Text>
+                </View>
+                <View style={{ flex: 0.1 }}>
+                    <Text>Here are some of the most rated bikes.</Text>
+                </View>
+                <View style={{ backgroundColor: '#F7F7F7', flex: 0.7 }}>
+                    {
+                        dataIsLoaded ?
+                            (
+                                <FlatList
+                                    keyExtractor={(item) => item._id}
+                                    data={data}
+                                    renderItem={renderItem}
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                />
+                            ) :
+                            (
+                                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+                                    <ActivityIndicator size="large" color='#CA054D' />
+                                </View>
+                            )
+                    }
+                </View>
             </View>
         </View>
     );
